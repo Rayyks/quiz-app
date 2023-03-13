7b6c35b2de72b0a530b26b96416745a0d43aed77
rayyks <rayydna14@gmail.com>
2023-03-13 18:02:33 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useStat, useEffect } from "react";
+
+import "./App.css";
+// Components Import
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App"></div>;
+}
+
+export default App;
